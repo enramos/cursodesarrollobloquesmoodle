bb92d4d02c8b25fbef4969935eba0b99e4322ec3
Enrique Ramos Ortiz <personal@enramos.es>
2022-08-01 12:08:38 +0200

    Versión inicial 0.01

---
+++ block_cursodesarrollobloquesmoodle.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Curso: desarrollo bloques Moodle
+ *
+ * @package   block_cursodesarrollobloquesmoodle
+ * @copyright 2022 Enrique Ramos Ortiz
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_cursodesarrollobloquesmoodle extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname', 'block_cursodesarrollobloquesmoodle');
+    }
+
+
+public function get_content() {
+    if ($this->content !== null) {
+      return $this->content;
+    }
+
+    $this->content         =  new stdClass;
+    $this->content->text   = 'Hola Mundo desde Moodle!';
+    $this->content->footer = 'Todos los derechos reservados';
+ 
+    return $this->content;
+}
+
+}
