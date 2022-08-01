534582305fb98a49afcc522cc4b1e4979ae40c8e
Enrique Ramos Ortiz <personal@enramos.es>
2022-08-01 13:31:55 +0200

    Cambio texto predeterminado

--- edit_form.php
+++ edit_form.php
@@ -31,7 +31,8 @@ class block_cursodesarrollobloquesmoodle_edit_form extends block_edit_form {
 
         // Agrega un campo de configuración de texto
         $mform->addElement('text', 'config_text', get_string('blockstring', 'block_cursodesarrollobloquesmoodle'));
-        $mform->setDefault('config_text', 'Cadena por omisión');
+        //En caso de que queramos agregarle un valor por defecto...
+        //$mform->setDefault('config_text', 'Cadena por omisión');
         $mform->setType('config_text', PARAM_RAW);        
 
     }
